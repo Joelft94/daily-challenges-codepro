b45a11ba808b785c2bfde5af5dc343ab4932c896
Joelft94 <joel_ferrer_94@hotmail.com>
2024-06-17 15:54:14 -0400

    add day5

--- day4.py
+++ day4.py
@@ -6,12 +6,3 @@ num = [int(i) for i in entrada.split(',')]
 lista = list(num)
 
 print(sorted(lista))
-
-
-
-
-# lista = list(map(int, num.split()))
-
-# lista_ascendente = sorted(lista)
-
-# print(lista_ascendente)
---
+++ day5.py
@@ -0,0 +1,7 @@
+list1 = ["perro" , "gato", "oveja", "vaca" , "chanchito"]
+
+list2= ["guau", "miau", "beee", "muu", "oink"]
+
+dict1 = dict(zip(list1, list2)) 
+
+print(dict1)
