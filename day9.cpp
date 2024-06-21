45f34b245e13820ffb785954ce6862d72be53e63
Joelft94 <joel_ferrer_94@hotmail.com>
2024-06-21 14:24:13 -0400

    add day 9

---
+++ day9.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+
+int main() {
+    std::cout << "Introduce el primer numero" << std::endl;
+    int a;
+    std::cin >> a;
+    std::cout << "Introduce el segundo numero" << std::endl;
+    int b;
+    std::cin >> b;
+    std::cout << "La suma es: " << a + b << std::endl;
+    return 0;
+}
+
